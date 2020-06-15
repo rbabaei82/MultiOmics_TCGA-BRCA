3dd6ecb1949b0d201e5001ecc8f37426cfff211b
Rohollah Babaei <rbabaei82@gmail.com>
2020-06-15 20:58:24 +0200

    exploratory analysis

--- code.R
+++ code.R
@@ -1,42 +1,131 @@
 library(curatedTCGAData)
-library(MultiAssayExperiment)
-library(GenomicRanges)
-library(RaggedExperiment)
-library(GenomicDataCommons)
-library(SummarizedExperiment)
-library(SingleCellExperiment)
-library(TCGAutils)
-library(UpSetR)
-library(mirbase.db)
-library(AnnotationFilter)
-library(EnsDb.Hsapiens.v86)
-library(survival)
-library(survminer)
+#library(MultiAssayExperiment)
+#library(GenomicRanges)
+#library(RaggedExperiment)
+#library(GenomicDataCommons)
+#library(SummarizedExperiment)
+#library(SingleCellExperiment)
+#library(TCGAutils)
+#library(UpSetR)
+#library(mirbase.db)
+#library(AnnotationFilter)
+#library(EnsDb.Hsapiens.v86)
+#library(survival)
+#library(survminer)
 library(pheatmap)
 library(ggplot2)
+library(tidyverse)
+library(tictoc)
+
 
 curatedTCGAData(diseaseCode = "BRCA")
-BRCAmae <- curatedTCGAData("BRCA", c("GISTIC_Peaks","Mutation", "RNASeq2GeneNorm", "RPPAArray"),
+BRCAmae <- curatedTCGAData("BRCA", c("GISTIC_ThresholdedByGene","Mutation", "RNASeq2GeneNorm", "RPPAArray"),
                            dry.run = FALSE)
 
-
+#####
 BRCAmae
 dim(colData(BRCAmae))
 head(colnames(colData(BRCAmae)))
 head(metadata(colData(BRCAmae))[["subtypes"]])
 colData(BRCAmae)[1:4,1:4]
-table(BRCAmae$histological_type)
+table(BRCAmae$histological_type)# infiltrating ductal and lobular carcinomas are the most prevalent type, we select only these for analysis
 
+## col_data for graph annotation
+dfcolData <- data.frame(matrix(nrow = nrow(colData(BRCAmae))))
+for(i in 1:ncol(colData(BRCAmae))){dfcolData <- cbind(dfcolData, BRCAmae@colData@listData[i])}
+dfcolData <- dfcolData[,-1]
+rownames(dfcolData) <- BRCAmae@colData@rownames
+colnames(dfcolData) <- gsub("\\-|\\_", ".", colnames(dfcolData))
+rownames(dfcolData) <- gsub("\\-|\\_", ".", rownames(dfcolData))
+
+# select patients with only ductal or lobular carcinoma
+dfcolData <- dfcolData %>% filter(histological.type == "infiltrating ductal carcinoma" | histological.type == "infiltrating lobular carcinoma")
+
+
+######
 summary(complete.cases(BRCAmae)) 
 listAssays <- assays(BRCAmae)
 
-Mutation <- listAssays[[1]]
+#######
+copyNumber <- listAssays[[1]]
+colnames(copyNumber) <- gsub("(\\-[[:alnum:]]+\\-[[:alnum:]]+\\-[[:alnum:]]+\\-[0-9]+)$", "", colnames(copyNumber))
+copyNumberdf <- as.data.frame(copyNumber)
+colnames(copyNumberdf) <- gsub("\\-|\\_", ".", colnames(copyNumberdf))
+
+# select patients with only ductal or lobular carcinoma
+copyNumberdf <- copyNumberdf[,intersect(colnames(copyNumberdf), rownames(dfcolData))]
+dim(copyNumberdf)
+length(unique(colnames(copyNumberdf))) ## there are unique variables
+sum(is.na(copyNumberdf)) # there is no missing value
+
+
+
+
+
+Mutation <- listAssays[[2]]
 colnames(Mutation) <- gsub("(\\-[[:alnum:]]+\\-[[:alnum:]]+\\-[[:alnum:]]+\\-[0-9]+)$", "", colnames(Mutation))
 
-RNASeq <- listAssays[[2]]
-colnames(RNASeq) <- gsub("(\\-[[:alnum:]]+\\-[[:alnum:]]+\\-[[:alnum:]]+\\-[0-9]+)$", "", colnames(RNASeq))
+# reshape the sparse matrix and set binary values for mutations
+Mutationdf <- as.data.frame(Mutation)
+tmp <- data.frame(GeneSymbol = rep(NA, nrow(Mutationdf)))
+for(i in 1:ncol(Mutationdf)){
+  tmp$GeneSymbol <- coalesce(Mutationdf[,i], tmp$GeneSymbol)
+  Mutationdf$GeneSymbol <- tmp$GeneSymbol
+}
 
-RPPA <- listAssays[[3]]
+sum(is.na(Mutationdf$GeneSymbol))# no missing value
+
+
+tmp <- data.frame(GeneSymbol = unique(Mutationdf$GeneSymbol))# select unique gene symbols
+sum(is.na(tmp$GeneSymbol))# no NA
+
+library(foreach)
+library(doParallel)
+cl <- makeCluster(3)
+registerDoParallel(cl)
+
+tmp_data <- foreach(i = c(1:993), j = c(1:993))%dopar%{
+  tmp[,paste0("V", i)] <- ifelse(tmp$GeneSymbol%in%Mutationdf[,j], 1, 0)
+}
+stopCluster(cl)
+
+tmp_df <- data.frame(matrix(unlist(tmp_data), ncol = length(tmp_data)))# convert to data frame
+colnames(tmp_df) <- colnames(Mutationdf[,1:993])
+rownames(tmp_df) <- tmp$GeneSymbol
+Mutationdf <- tmp_df
+colnames(Mutationdf) <- gsub("\\-|\\_", ".", colnames(Mutationdf))
+
+# select patients with only ductal or lobular carcinoma
+Mutationdf <- Mutationdf[,intersect(colnames(Mutationdf), rownames(dfcolData))]
+dim(Mutationdf)
+
+length(unique(colnames(Mutationdf)))# unique variables
+rm(tmp, tmp_data, tmp_df)
+
+
+
+
+RNASeq <- listAssays[[3]]
+colnames(RNASeq) <- gsub("(\\-[[:alnum:]]+\\-[[:alnum:]]+\\-[[:alnum:]]+\\-[0-9]+)$", "", colnames(RNASeq))
+# log transformation
+RNASeq <- log2(RNASeq +1)
+
+RNASeqdf <- as.data.frame(RNASeq)
+
+
+dim(RNASeqdf)
+length(unique(colnames(RNASeqdf)))# there are duplicated variables
+
+RNASeqdf <- RNASeqdf[,unique(colnames(RNASeqdf))] # remove duplicated variables
+colnames(RNASeqdf) <- gsub("\\-|\\_", ".", colnames(RNASeqdf))
+
+# select patients with only ductal or lobular carcinoma
+RNASeqdf <- RNASeqdf[,intersect(colnames(RNASeqdf), rownames(dfcolData))]
+dim(RNASeqdf)
+
+
+
+RPPA <- listAssays[[4]]
 dfRPPA <- data.frame(matrix(nrow = 226))
 for(i in 1:937){dfRPPA <- cbind(dfRPPA, RPPA@listData[i])}
 dfRPPA <- dfRPPA[,-1]
@@ -44,11 +133,69 @@ rownames(dfRPPA) <- RPPA@rownames
 colnames(dfRPPA) <- gsub("(\\-[[:alnum:]]+\\-[[:alnum:]]+\\-[[:alnum:]]+\\-[0-9]+)$", "", colnames(dfRPPA))
 rownames(dfRPPA) <- gsub("\\-|\\_", ".", rownames(dfRPPA))
 
+length(unique(colnames(dfRPPA)))# there are duplicated variables
+dfRPPA <- dfRPPA[,unique(colnames(dfRPPA))]
+colnames(dfRPPA) <- gsub("\\-|\\_", ".", colnames(dfRPPA))
 
-copyNumber <- listAssays[[4]]
-colnames(copyNumber) <- gsub("(\\-[[:alnum:]]+\\-[[:alnum:]]+\\-[[:alnum:]]+\\-[0-9]+)$", "", colnames(copyNumber))
+# select patients with only ductal or lobular carcinoma
+dfRPPA <- dfRPPA[,intersect(colnames(dfRPPA), rownames(dfcolData))]
+dim(dfRPPA)
 
 
+## list of data frames with same variables
+dataList <- list(CN =copyNumberdf,MU= Mutationdf,RS= RNASeqdf,PP= dfRPPA)
+intersectID <- Reduce(intersect, lapply(dataList, colnames))
+dataList <- lapply(dataList, function(x) x[intersectID])
+
+## filter colData by shared variables
+dfcolData <- dfcolData[intersect(rownames(dfcolData), colnames(dataList$CN)),]
+
+## list of data frames with same genes (excluding RPPA)
+omicsList <- dataList[1:3]
+intersectID <- Reduce(intersect, lapply(omicsList, rownames))
+omicsList <- lapply(omicsList, function(x) x[intersectID,])
+
+dfRPPA <- dataList$PP
+
+save(omicsList, dfRPPA, dfcolData, file = "Data.RData")
+
+###
+# clustering in relation to histological types
+anno_col <- data.frame(type = as.factor(dfcolData$histological.type))
+rownames(anno_col) <- rownames(dfcolData)
+
+### heatmap of gene expression
+tic("heat map GE")
+pheatmap(as.matrix(omicsList$RS), annotation_col = anno_col,
+         show_rownames = F, show_colnames =  F, main = "Gene Expression") # clustered by genes , not by subtypes
+
+toc() # 34 minutes
+
+
+# pheatmap(as.matrix(omicsList$MU), annotation_col = anno_col,
+#         show_rownames = F, show_colnames =  F, main = "Mutation")
+
+pheatmap(as.matrix(omicsList$CN), annotation_col = anno_col,
+         show_rownames = F, show_colnames =  F, main = "Copy Number")
+
+
+
+# correlation between gene expression and copy number
+corr <- cor(t(omicsList$CN), t(omicsList$RS))
+which.max(diag(corr))
+
+# top ten correlated genes
+top10 <- order(diag(corr), decreasing = T)[1:10]
+k <- arrayInd(top10, dim(corr))
+topGenes <- rownames(corr)[k[,1]]
+topGenes
+# boxplot of the top gene
+dfTop <- data.frame(RNASeq = t(omicsList$RS[topGenes[1],]), CopyNumber = t(omicsList$CN[topGenes[1],]))
+colnames(dfTop) <- c("RNASeq", "CopyNumber")
+boxplot(RNASeq~CopyNumber, data = dfTop, xlab = "GISTIC Relative Copy Number Call", ylab = "RNA-Seq log FC", main = "KIAA1967")
+
+
+################################
 
 brcaMatched <- intersectColumns(BRCAmae) # select complete cases and rearrange each ExperimentList element so its columns correspond exactly to rows of colData in the same order
 colnames(brcaMatched)
